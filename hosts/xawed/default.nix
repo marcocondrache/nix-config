48b711de648b686751c3b269fa102f22a31f43cd
marcocondrache <marcomihaicondrache@gmail.com>
2025-01-12 20:53:59 +0100

    feat: add dock configuration to xawed Nix setup
    - Introduced default dock settings in xawed.default.nix to enhance user interface experience.
    - Configured dock to autohide, set orientation to bottom, and adjusted various display options for improved usability.

--- hosts/xawed/default.nix
+++ hosts/xawed/default.nix
@@ -12,5 +12,16 @@
 
   system = {
     stateVersion = 5;
+
+    defaults = {
+      dock = {
+        autohide = true;
+        orientation = "bottom";
+        magnification = false;
+        mineffect = "genie";
+        minimize-to-application = false;
+        show-recents = false;
+      };
+    };
   };
 }
