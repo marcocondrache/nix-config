872b30b51f5f41caf8ce2be4a1049ee87a517922
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-01-22 11:15:54 +0100

    fix: correct nixpkgs URL format in flake.nix for improved package sourcing

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Marco's system configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
