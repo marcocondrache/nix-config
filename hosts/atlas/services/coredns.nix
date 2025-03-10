6914e17e87f6b5f7fe996371ddd8f1af0bdedbac
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-03-10 19:47:31 +0100

    fix: coredns resolution

--- hosts/atlas/services/coredns.nix
+++ hosts/atlas/services/coredns.nix
@@ -6,6 +6,9 @@
       ts.marcocondrache.com {
         rewrite name regex (.*)\.ts\.marcocondrache\.com {1}.tail13f4af.ts.net
         forward . 100.100.100.100
+        
+        substitute response tail13f4af.ts.net ts.marcocondrache.com
+        
         cache
       }
     '';
