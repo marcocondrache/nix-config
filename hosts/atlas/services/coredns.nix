9effa5ece0ba4412818ad5e72e4204e161e0dd4b
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-03-10 15:44:12 +0100

    fix: tailnet domain

--- hosts/atlas/services/coredns.nix
+++ hosts/atlas/services/coredns.nix
@@ -4,7 +4,7 @@
 
     config = ''
       ts.marcocondrache.com {
-        rewrite name regex (.*)\.ts\.marcocondrache\.com {1}.tailnet.com
+        rewrite name regex (.*)\.ts\.marcocondrache\.com {1}.tail13f4af.ts.net
         forward . 100.100.100.100
         cache
       }
