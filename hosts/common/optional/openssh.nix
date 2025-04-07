897a6a0fece359c815c5c6a9de980c704d115559
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-04-07 21:30:05 +0200

    fix: openssh

--- hosts/common/optional/openssh.nix
+++ hosts/common/optional/openssh.nix
@@ -13,14 +13,14 @@
       StreamLocalBindUnlink = "yes";
       GatewayPorts = "clientspecified";
     };
-  };
 
-  hostKeys = [
-    {
-      path = "/persist/etc/ssh/ssh_host_ed25519_key";
-      type = "ed25519";
-    }
-  ];
+    hostKeys = [
+      {
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
+  };
 
   security.pam.sshAgentAuth = {
     enable = true;
