d2d94e2fe5a1cddc858a1d848056ab7b8a3d9e0d
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-05-08 20:43:43 +0200

    feat: update lock and configure bun

--- home/marcocondrache/features/cli/bun.nix
+++ home/marcocondrache/features/cli/bun.nix
@@ -3,5 +3,19 @@
   programs.bun = {
     enable = lib.mkDefault true;
     enableGitIntegration = lib.mkDefault true;
+    settings = {
+      telemetry = false;
+
+      install = {
+        # Avoid polluting the home directory
+        globalDir = "/tmp/bun/global";
+        globalBinDir = "/tmp/bun/global/bin";
+      };
+
+      run = {
+        bun = true;
+        silent = true;
+      };
+    };
   };
 }
