15635a40ce4b517659e5c9cf0066546de0f9a6f4
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-07-01 13:37:33 +0200

    chore: use default values
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- home/marcocondrache/features/cli/gpg.nix
+++ home/marcocondrache/features/cli/gpg.nix
@@ -34,8 +34,6 @@ in
     enableSshSupport = true;
     enableExtraSocket = true;
     sshKeys = [ "8A4248ED29B2E49BEA8D6D2560CD3EBBEADCE11D" ];
-    maxCacheTtl = 120;
-    defaultCacheTtl = 60;
   };
 
   systemd.user.services = lib.mkIf (!darwin) {
