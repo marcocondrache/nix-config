1af45899d89faca4b179490feea43e537fd813aa
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-07-15 11:29:40 +0200

    fix: kustomize k9s
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- home/marcocondrache/features/cli/k9s.nix
+++ home/marcocondrache/features/cli/k9s.nix
@@ -5,5 +5,14 @@
 {
   programs.k9s = {
     enable = lib.mkDefault true;
+    settings = {
+      k9s = {
+        liveViewAutoRefresh = true;
+        refreshRate = 1;
+        ui = {
+          headless = true;
+        };
+      };
+    };
   };
 }
