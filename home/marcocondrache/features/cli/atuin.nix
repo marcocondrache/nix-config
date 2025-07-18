e2920a1a02e1ddf42522987e3b6432a6a501bcfb
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-07-18 20:48:05 +0200

    fix: atuin
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- home/marcocondrache/features/cli/atuin.nix
+++ home/marcocondrache/features/cli/atuin.nix
@@ -1,5 +1,10 @@
 {
-    programs.atuin = {
-        enable = true;
+  programs.atuin = {
+    enable = true;
+    settings = {
+      auto_sync = true;
+      sync_address = "https://sh.marcocondrache.com";
+      update_check = false;
     };
+  };
 }
