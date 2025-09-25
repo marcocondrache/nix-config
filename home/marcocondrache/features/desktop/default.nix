86188384a11d01979ff02c48b1e954fa500e6a9f
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2025-09-25 18:59:03 +0200

    chore: desktop apps are trash with nix
    Signed-off-by: Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>

--- home/marcocondrache/features/desktop/default.nix
+++ home/marcocondrache/features/desktop/default.nix
@@ -1,9 +1,9 @@
 {
   imports = [
     ./brave.nix
-    ./spotify.nix
-    ./raycast.nix
-    ./postman.nix
+    # ./spotify.nix
+    # ./raycast.nix
+    # ./postman.nix
     ./ghostty.nix
     ./fonts.nix
   ];
