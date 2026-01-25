0d7cdc0ae7e17c436ebccd1e921641e0facc6230
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-01-25 14:21:33 +0100

    improve zoxide setup

--- home/marcocondrache/features/cli/fish.nix
+++ home/marcocondrache/features/cli/fish.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }:
@@ -15,12 +16,23 @@ in
       g = "git";
       ce = "zed .";
       ls = lib.mkIf (hasPackage "eza") "eza";
-      cd = lib.mkIf (hasPackage "zoxide") "z";
       kf = lib.mkIf (hasPackage "kubef") "kubef";
       lg = lib.mkIf (hasPackage "lazygit") "lazygit";
       grep = lib.mkIf (hasPackage "ripgrep") "rg";
       exa = ls;
     };
+
+    plugins =
+      [ ]
+      ++ lib.optional (hasPackage "zoxide") {
+        name = "zoxide";
+        src = pkgs.fetchFromGitHub {
+          owner = "icezyclon";
+          repo = "zoxide.fish";
+          rev = "27a058a661e2eba021b90e9248517b6c47a22674";
+          hash = "sha256-OjrX0d8VjDMxiI5JlJPyu/scTs/fS/f5ehVyhAA/KDM=";
+        };
+      };
 
     functions = {
       fish_greeting = "";
