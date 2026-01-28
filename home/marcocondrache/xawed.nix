36f437a5c50a734b4a3ce06c1e533555f9c54662
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-01-28 08:36:16 +0100

    flake update

--- home/marcocondrache/xawed.nix
+++ home/marcocondrache/xawed.nix
@@ -24,6 +24,10 @@ in
       contents = {
         feature.manyFiles = true;
         url."git@danfoss.github.com:".insteadOf = "git@github.com:";
+
+        # Use SSH signatures instead of GPG for work repos
+        gpg.format = "ssh";
+        user.signingKey = "${workEnv}/.ssh/id_work_sk";
       };
     }
 
