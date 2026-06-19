dbda4da3f09ae17e3fee10d9a5d3758fe54a06a6
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-06-19 20:32:57 +0200

    chore: Add some extra caches

--- hosts/common/optional/determinate.nix
+++ hosts/common/optional/determinate.nix
@@ -5,6 +5,14 @@
     enable = true;
     customSettings = {
       eval-cores = 0;
+      extra-substituters = [
+        "https://zed.cachix.org"
+        "https://devenv.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      ];
     };
   };
 }
