1eb5f913f163f1080d3cf83843f94295982d32da
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-06-21 20:54:57 +0200

    chore: We don't need zed here

--- hosts/common/optional/determinate.nix
+++ hosts/common/optional/determinate.nix
@@ -6,12 +6,6 @@
     customSettings = {
       eval-cores = 0;
       trusted-users = [ "marcocondrache" ];
-      extra-substituters = [
-        "https://zed.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "zed.cachix.org-1:/pHQ6dpMsAZk2DiP4WCL0p9YDNKWj2Q5FL20bNmw1cU="
-      ];
     };
   };
 }
