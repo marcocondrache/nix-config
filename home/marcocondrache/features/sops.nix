c384bc6dd40e2cd75b837bd2186007e8ebab4684
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-09-03 16:41:01 +0200

    fix: Always restart sops agent

--- home/marcocondrache/features/sops.nix
+++ home/marcocondrache/features/sops.nix
@@ -1,8 +1,14 @@
+{ lib, ... }:
+
 {
   sops = {
     defaultSopsFile = ../secrets.yaml;
     defaultSymlinkPath = "/Users/marcocondrache/.secrets";
     gnupg.home = "/Users/marcocondrache/.gnupg";
     gnupg.sshKeyPaths = [ ];
+  };
+
+  launchd.agents.sops-nix.config.KeepAlive = lib.mkForce {
+    SuccessfulExit = false;
   };
 }
