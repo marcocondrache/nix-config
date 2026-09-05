adfbc32481f9f76a9220408c66caf20c63b6fd29
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-09-05 10:33:56 +0200

    chore: Allow mise to authenticate

--- home/marcocondrache/features/mise.nix
+++ home/marcocondrache/features/mise.nix
@@ -1,6 +1,15 @@
+{ pkgs, lib, ... }:
 {
   programs.mise = {
     enable = true;
     enableFishIntegration = true;
+
+    globalConfig = {
+      settings = {
+        github = {
+          credential_command = "${lib.getExe pkgs.gh} auth token";
+        };
+      };
+    };
   };
 }
